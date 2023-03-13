05916aa38fcd30863c3d9410a8d57807a789171d
Hanson Nguyen <64661526+hansonguyen@users.noreply.github.com>
2023-03-13 00:38:38 -0700

    edit images

--- data/projects.js
+++ data/projects.js
@@ -1,10 +1,14 @@
+import workoutImg from "../assets/workoutImg.png";
+import botImg from "../assets/botImg.png";
+import climateImg from "../assets/climateImg.png";
+
 export default [
     {
         id: 1,
         title: "Workout Tracker",
         description: "Workout Tracker is a full-stack website to create and customize personal workout plans. I implemented user authentication with JSON Web Tokens and created API endpoints to create and manipulate data.",
         skills: ["React", "Node.js", "Express", "MongoDB"],
-        imgURL: "../assets/undraw_Personal_trainer_re_cnua.png",
+        imgURL: workoutImg,
         link: "https://workouttracker-e1uh.onrender.com/"
     },
     {
@@ -12,7 +16,7 @@ export default [
         title: "Pokébot",
         description: "Designed and developed a Discord bot that retrieves data from the RESTful PokéAPI. Using Python and OOP principles, I created commands to present the API data to users.",
         skills: ["Python", "REST APIs"],
-        imgURL: "../assets/undraw_Chat_bot_re_e2gj.png",
+        imgURL: botImg,
         link: "https://github.com/hansonguyen/discord-bot"
     },
     {
@@ -20,7 +24,7 @@ export default [
         title: "Climate Change Project",
         description: "Created a prototype for a carbon absorption system using zeolite for incinerators with a team. I used an Arduino and C++ to build a circuit involving a temperature sensor, motors, and LEDs to control a fan.",
         skills: ["C", "Arduino", "Circuits"],
-        imgURL: "../assets/undraw_environment_iaus.png",
+        imgURL: climateImg,
         link: "https://docs.google.com/presentation/d/1xlxHoScmQTxYbLFpoFev-iPjkHxcrbj0u5Y_0gwYWtg/edit?usp=sharing"
     },
 ]
