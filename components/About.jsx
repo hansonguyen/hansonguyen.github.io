ff8385285adaa05203ca76db058f73a264f113f8
Hanson Nguyen <64661526+hansonguyen@users.noreply.github.com>
2023-03-13 00:03:52 -0700

    updated projects

--- components/About.jsx
+++ components/About.jsx
@@ -20,8 +20,8 @@ export default function About() {
                     initial={{opacity: 0}}
                     whileInView={{opacity: 1}}
                 >
-                    <p>As a second-year computer engineering student at UC Davis with a minor in technology management, I am constantly seeking opportunities to apply the knowledge and skills I am acquiring in the classroom to real-world projects. I am a proactive problem-solver, always eager to learn and grow as a coder, engineer, and individual.</p>
-                    <p>In my spare time, I enjoy spending time with my loved ones, working out, and experimenting with new cooking recipes.</p>
+                    <p>As a second-year <strong>Computer Engineering</strong> student at UC Davis with a minor in Technology Management, I am constantly seeking opportunities to apply the knowledge and skills I am acquiring in the classroom to real-world projects. I am a proactive problem-solver, always eager to learn and grow as a coder, engineer, and individual.</p>
+                    <p>I am currently working on with <strong>AggieWorks</strong> to build a roommate matching app using <strong>React Native</strong>, <strong>TypeScript</strong>, and <strong>Supabase</strong>.</p>
                 </motion.div>
             </div>
         </section>
--- components/Card.jsx
+++ components/Card.jsx
@@ -1,10 +1,14 @@
-import React from "react"
+import React from 'react'
 
 export default function Card(props) {
+    const skills = props.skills.map((skill) => {
+        return <p className="card--skill">{skill}</p>
+    })
     return (
         <div className="card--container">
-            <img className="card--image" src={props.imgURL}/>
+            <img className="card--image" src={props.imgURL} />
             <h3 className="card--title">{props.title}</h3>
+            <div className="card--skills">{skills}</div>
             <p className="card--description">{props.description}</p>
             <a target="_blank" href={props.link} className="card--link">
                 <button className="card--button">See more</button>
